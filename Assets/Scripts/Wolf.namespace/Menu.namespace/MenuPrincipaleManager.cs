25ca77bab247092f8e80d020485b15516e44ce40
production-jeu <43345835+production-jeu@users.noreply.github.com>
2018-10-19 09:00:03 -0400

    Menu

--- Assets/Scripts/Wolf.namespace/Menu.namespace/MenuPrincipaleManager.cs
+++ Assets/Scripts/Wolf.namespace/Menu.namespace/MenuPrincipaleManager.cs
@@ -8,13 +8,27 @@ namespace Wolf.Menu
     public class MenuPrincipaleManager : MonoBehaviour
     {
         Animator animator;
+        public RectTransform selecteur;
+        public Vector2 bond = new Vector2(0, 118);
+        public int indexSelecteurMin = 0;
+        public int indexSelecteurMax = 2;
+        Vector2 posInitialeSelecteur;
+        int indexSelecteur = 0;
         private void Awake()
         {
             animator = GetComponent<Animator>();
         }
         private void Start()
         {
-            animator.Play("anim_menu_ouverture", -1, 0);
+            //animator.Play("anim_menu_ouverture", -1, 0);
+            posInitialeSelecteur = selecteur.anchoredPosition;
+        }
+        private void Update()
+        {
+            if (Input.GetButtonDown(KeyCode.W) || Input.GetButtonDown(KeyCode.UpArrow) || Input.GetButtonDown(KeyCode.DownArrow)) {
+                indexSelecteur += (int)Input.GetAxisRaw("Vertical");
+                selecteur.anchoredPosition += new Vector2(0, 118);
+            }
         }
         public void CommencerJeu()
         {
